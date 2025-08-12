64652fc2b84803539124b75cd8ec704f7851a657
AlkAlkaabi907 <Alkaabi454@gmail.com>
2025-08-12 10:34:15 +0300

    done

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,20 +1,14 @@
 function bubbleSort(array) {
-    // Make some sort of loop that keeps happening if there were any swaps
-    // that happened this turn
-
-    // reset swapHappened to false each time so we can detect if a swap
-    // happened in this specific iteration.
-
-    // Make another loop (inside the first one) to go through one 
-    // round of swapping from the start of the list to the end
-    
-    // Inside inner loop:
-    // compare each pair of elements near each other
-    // swap them if the bigger one was at a lower index.
-
-    // Make sure to keep track of whether a swap happened!
-
-    // After both loops have exited, remember to return the array
+function bubbleSort(array) {
+    for (i=0; i < array.length; i++) {
+        for (j=1; j < array.length -i; j++) {
+            if (array[j-1] > array[j]) {
+                [array[j-1], array[j]] = [array[j], array[j-1]]
+            }
+        }
+    }
+    return array;
+}
     return array;
 }
 
